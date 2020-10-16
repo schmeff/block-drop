3f31d45ebcbacd2f37405ceb5757f01ccd7cb99c
Jeff Elsey <jcelsey@gmail.com>
2020-10-16 14:08:37 -0600

    BD-31 - Added another 7 levels to 6x6

--- lib/data/levels_data.dart
+++ lib/data/levels_data.dart
@@ -1241,6 +1241,266 @@ class LevelsData {
         "blockCount": 3,
         "stars": {"one": 6, "two": 8, "three": 10},
       },
+      8: {
+        "number": 8,
+        "levelDimensions": {"rows": 6, "columns": 6},
+        "enemies": [
+          {
+            "id": 0,
+            "position": {"row": 2, "column": 2},
+            "status": 1,
+            "route": [3, 2]
+          },
+          {
+            "id": 1,
+            "position": {"row": 3, "column": 1},
+            "status": 1,
+            "route": [3, 3, 3, 2, 2, 2]
+          },
+          {
+            "id": 2,
+            "position": {"row": 4, "column": 0},
+            "status": 1,
+            "route": [3, 3, 3, 3, 3, 2, 2, 2, 2, 2]
+          },
+          {
+            "id": 3,
+            "position": {"row": 5, "column": 2},
+            "status": 1,
+            "route": [2, 2, 3, 3]
+          },
+          {
+            "id": 4,
+            "position": {"row": 5, "column": 3},
+            "status": 1,
+            "route": [3, 3, 2, 2]
+          },
+        ],
+        "blockCount": 3,
+        "stars": {"one": 1, "two": 2, "three": 3},
+      },
+      9: {
+        "number": 9,
+        "levelDimensions": {"rows": 6, "columns": 6},
+        "enemies": [
+          {
+            "id": 0,
+            "position": {"row": 2, "column": 0},
+            "status": 1,
+            "route": [3, 1, 3, 2, 0, 2]
+          },
+          {
+            "id": 1,
+            "position": {"row": 2, "column": 5},
+            "status": 1,
+            "route": [2, 1, 2, 3, 0, 3]
+          },
+          {
+            "id": 2,
+            "position": {"row": 3, "column": 0},
+            "status": 1,
+            "route": [1, 3, 3, 2, 2, 0]
+          },
+          {
+            "id": 3,
+            "position": {"row": 3, "column": 5},
+            "status": 1,
+            "route": [1, 2, 2, 3, 3, 0]
+          },
+        ],
+        "blockCount": 3,
+        "stars": {"one": 1, "two": 2, "three": 3},
+      },
+      10: {
+        "number": 10,
+        "levelDimensions": {"rows": 6, "columns": 6},
+        "enemies": [
+          {
+            "id": 0,
+            "position": {"row": 5, "column": 0},
+            "status": 1,
+            "route": [0, 3, 0, 3, 0, 3, 1, 2, 1, 2, 1, 2]
+          },
+          {
+            "id": 1,
+            "position": {"row": 3, "column": 4},
+            "status": 1,
+            "route": [3, 2]
+          },
+          {
+            "id": 2,
+            "position": {"row": 4, "column": 3},
+            "status": 1,
+            "route": [3, 3, 2, 2]
+          },
+          {
+            "id": 3,
+            "position": {"row": 5, "column": 2},
+            "status": 1,
+            "route": [3, 3, 3, 2, 2, 2]
+          },
+        ],
+        "blockCount": 3,
+        "stars": {"one": 1, "two": 2, "three": 3},
+      },
+      11: {
+        "number": 11,
+        "levelDimensions": {"rows": 6, "columns": 6},
+        "enemies": [
+          {
+            "id": 0,
+            "position": {"row": 3, "column": 0},
+            "status": 1,
+            "route": [3, 2]
+          },
+          {
+            "id": 1,
+            "position": {"row": 3, "column": 2},
+            "status": 1,
+            "route": [3, 2]
+          },
+          {
+            "id": 2,
+            "position": {"row": 3, "column": 4},
+            "status": 1,
+            "route": [3, 2]
+          },
+          {
+            "id": 3,
+            "position": {"row": 4, "column": 0},
+            "status": 1,
+            "route": [3, 1, 0, 3, 2, 1, 0, 2]
+          },
+          {
+            "id": 4,
+            "position": {"row": 4, "column": 5},
+            "status": 1,
+            "route": [2, 1, 0, 2, 3, 1, 0, 3]
+          },
+        ],
+        "blockCount": 3,
+        "stars": {"one": 1, "two": 2, "three": 3},
+      },
+      12: {
+        "number": 12,
+        "levelDimensions": {"rows": 6, "columns": 6},
+        "enemies": [
+          {
+            "id": 0,
+            "position": {"row": 2, "column": 2},
+            "status": 1,
+            "route": [1, 2, 2, 3, 3, 0]
+          },
+          {
+            "id": 1,
+            "position": {"row": 2, "column": 3},
+            "status": 1,
+            "route": [1, 3, 3, 2, 2, 0]
+          },
+          {
+            "id": 2,
+            "position": {"row": 2, "column": 0},
+            "status": 1,
+            "route": [3, 2]
+          },
+          {
+            "id": 3,
+            "position": {"row": 2, "column": 5},
+            "status": 1,
+            "route": [2, 3]
+          },
+          {
+            "id": 4,
+            "position": {"row": 5, "column": 2},
+            "status": 1,
+            "route": [0, 2, 2, 3, 3, 1]
+          },
+          {
+            "id": 5,
+            "position": {"row": 5, "column": 3},
+            "status": 1,
+            "route": [0, 3, 3, 2, 2, 1]
+          },
+        ],
+        "blockCount": 3,
+        "stars": {"one": 1, "two": 2, "three": 3},
+      },
+      13: {
+        "number": 13,
+        "levelDimensions": {"rows": 6, "columns": 6},
+        "enemies": [
+          {
+            "id": 0,
+            "position": {"row": 4, "column": 0},
+            "status": 1,
+            "route": [3, 3, 3, 3, 3, 1, 2, 2, 2, 2, 2, 0]
+          },
+          {
+            "id": 1,
+            "position": {"row": 4, "column": 3},
+            "status": 1,
+            "route": [3, 3, 1, 2, 2, 2, 2, 2, 0, 3, 3, 3]
+          },
+          {
+            "id": 2,
+            "position": {"row": 5, "column": 2},
+            "status": 1,
+            "route": [2, 2, 0, 3, 3, 3, 3, 3, 1, 2, 2, 2]
+          },
+          {
+            "id": 3,
+            "position": {"row": 5, "column": 5},
+            "status": 1,
+            "route": [2, 2, 2, 2, 2, 0, 3, 3, 3, 3, 3, 1]
+          },
+        ],
+        "blockCount": 3,
+        "stars": {"one": 1, "two": 2, "three": 3},
+      },
+      14: {
+        "number": 14,
+        "levelDimensions": {"rows": 6, "columns": 6},
+        "enemies": [
+          {
+            "id": 0,
+            "position": {"row": 4, "column": 0},
+            "status": 1,
+            "route": [1, 3, 0, 2]
+          },
+          {
+            "id": 1,
+            "position": {"row": 4, "column": 2},
+            "status": 1,
+            "route": [1, 3, 0, 2]
+          },
+          {
+            "id": 2,
+            "position": {"row": 4, "column": 4},
+            "status": 1,
+            "route": [1, 3, 0, 2]
+          },
+          {
+            "id": 3,
+            "position": {"row": 5, "column": 1},
+            "status": 1,
+            "route": [0, 2, 1, 3]
+          },
+          {
+            "id": 4,
+            "position": {"row": 5, "column": 3},
+            "status": 1,
+            "route": [0, 2, 1, 3]
+          },
+          {
+            "id": 5,
+            "position": {"row": 5, "column": 5},
+            "status": 1,
+            "route": [0, 2, 1, 3]
+          },
+        ],
+        "blockCount": 3,
+        "stars": {"one": 1, "two": 2, "three": 3},
+      },
     },
     "7x7": {},
     "8x8": {},
